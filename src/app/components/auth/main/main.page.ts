0fd350ee4f64a9da23bd3fdf56189b5d60586d67
erlyn23 <erlynetv@gmail.com>
2021-04-29 21:54:52 -0400

    Se terminó la primera vista

--- src/app/components/auth/main/main.page.ts
+++ src/app/components/auth/main/main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  goToPage(page: string){
+    this.router.navigate([`/${page}`]);
   }
 
 }
