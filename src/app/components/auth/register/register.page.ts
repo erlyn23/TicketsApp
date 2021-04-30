9356a8239650b060b70d544d8556366b613cd90c
erlyn23 <erlynetv@gmail.com>
2021-04-30 00:21:54 -0400

    Se añadieron validaciones al registro de usuarios

--- src/app/components/auth/register/register.page.ts
+++ src/app/components/auth/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  passwordPattern: RegExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])");
+  emailPattern: RegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm():void{
+    this.registerForm = this.formBuilder.group({
+      fullName: ["", [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      phone: ["",[Validators.required, Validators.minLength(9), Validators.maxLength(20), Validators.pattern("[0-9]*")]],
+      email: ["", [Validators.required, Validators.pattern(this.emailPattern)]],
+      password: ["", [Validators.required, Validators.minLength(8), Validators.maxLength(16), Validators.pattern(this.passwordPattern)]],
+      confirmPassword: ["", [Validators.required, Validators.minLength(8), Validators.maxLength(16), Validators.pattern(this.passwordPattern)]]
+    });
+  }
+
+  isValidField(field: string): string{
+    const validField: AbstractControl = this.registerForm.get(field);
+
+    return (!validField.valid && validField.touched) ? 'invalid-field' : validField.touched ? 'valid-field' : '';
+  }
+
+  verifyPasswords(event: CustomEvent): string{
+    const password = this.registerForm.value.password;
+    const confirmPassword = this.registerForm.value.confirmPassword; 
+    if(password != "" || confirmPassword != "")
+     return (password != confirmPassword) ? 'invalid-field' : 'valid-field';
   }
 
 }
