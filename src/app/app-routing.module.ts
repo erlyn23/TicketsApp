55bd87a9dd05c244053ff2d5dccef3ad510e24c1
erlyn23 <erlynetv@gmail.com>
2021-04-29 20:02:11 -0400

    Se inicializó el repositorio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./components/auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./components/auth/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./components/auth/main/main.module').then( m => m.MainPageModule)
   },
 ];
 
