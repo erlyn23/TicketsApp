70f7bf9c4f4fb97221c3768a4f0aaf3fc5c8d1f2
erlyn23 <erlynetv@gmail.com>
2021-05-19 21:00:32 -0400

    Se terminó el home de la app

--- src/app/components/dashboard/customer/home/home.component.ts
+++ src/app/components/dashboard/customer/home/home.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireObject } from '@angular/fire/database';
+import { NavigationExtras, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IBusiness } from 'src/app/core/models/business.interface';
+import { IUser } from 'src/app/core/models/user.interface';
+import { AuthService } from 'src/app/services/auth.service';
+import { RepositoryService } from 'src/app/services/repository.service';
+import { UtilityService } from 'src/app/services/utility.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +15,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  userPhoto: string = "";
+  userName: string = "";
 
-  ngOnInit() {}
+  userUid: string = "";
+
+  navExtras: NavigationExtras = { state: { business: null, clientsInTurn: 0 } };
+
+  constructor(private authService: AuthService, 
+    private repositoryService: RepositoryService<IUser>,
+    private businessRepoService: RepositoryService<IBusiness[]>,
+    private utilityService: UtilityService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.userUid = this.authService.userData.uid;
+
+    this.getUserData();
+    this.getTopThree();
+  }
+
+  user$: Subscription;
+  getUserData(){
+    const angularFireObject: AngularFireObject<IUser> = this.repositoryService.getAllElements(`users/${this.userUid}`);
+    this.user$ = angularFireObject.valueChanges().subscribe(result=>{
+      this.userPhoto = result.photo;
+      this.userName = result.fullName;
+    });
+  }
+  businessTopThreeList: IBusiness[] = [];
+  businessObject: AngularFireObject<IBusiness[]>;
+  business$: Subscription;
+  getTopThree(){
+    this.businessObject = this.businessRepoService.getAllElements(`businessList`);
+    this.business$ = this.businessObject.snapshotChanges().subscribe(result=>{
+      const businessList = result.payload.val();
+      this.businessTopThreeList = [];
+      if(businessList.length > 3){
+        let sortable = [];
+        for(let businessKey in businessList){
+          businessList[businessKey].key = businessKey;
+          sortable.push([businessKey, businessList[businessKey].clientsInTurn]);
+        }
+        sortable.sort((a, b)=>{
+          return b[1] - a[1];
+        });
+
+        for(let businessKey in businessList){
+          if(sortable[0][0] === businessKey) this.businessTopThreeList[0] = businessList[businessKey];
+          if(sortable[1][0] === businessKey) this.businessTopThreeList[1] = businessList[businessKey];
+          if(sortable[2][0] === businessKey) this.businessTopThreeList[2] = businessList[businessKey];
+        }
+      }else{
+
+        for(let businessKey in businessList){
+          businessList[businessKey].key = businessKey;
+          this.businessTopThreeList.push(businessList[businessKey]);
+        }
+      }
+    });
+  }
+
+  async addBusinessToFavouriteList(business: IBusiness){
+    await this.repositoryService.setElement(`favourites/${this.userUid}/${business.key}`, {businessKey: business.key}).then(async ()=>{
+      await this.utilityService.presentToast('Negocio añadido a favoritos', 'success-toast');
+    }).catch(async err=>{
+      console.log(err);
+      await this.utilityService.presentToast('Ha ocurrido un error al añadir a favoritos', 'error-toast');
+    });
+  }
+
+  goToBusinessDetails(business: IBusiness){
+    this.navExtras.state.business = business;
+    this.navExtras.state.clientsInTurn = business.clientsInTurn;
+    this.router.navigate(['/business-details'], this.navExtras);
+  }
+
+  ngOnDestroy() {
+    this.user$.unsubscribe();
+    this.business$.unsubscribe();
+  }
 
 }
