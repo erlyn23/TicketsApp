b822c3efb3fe0518bce96b7675082a4567393d6b
erlyn23 <erlynetv@gmail.com>
2021-09-03 20:58:29 -0400

    Se añadieron los accesos de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC7ObVf0qluPJFUWnu6UFJ8UDeIdUvggmk",
+    authDomain: "tuturnord-85d8f.firebaseapp.com",
+    projectId: "tuturnord-85d8f",
+    storageBucket: "tuturnord-85d8f.appspot.com",
+    messagingSenderId: "899090511952",
+    appId: "1:899090511952:web:8024c9140df54a3d943955"
+  }
 };
